8679dd4e5aef8df7e33c91d63ef435bd22c4c9b0
ReneKubax <97212849+ReneKubax@users.noreply.github.com>
2022-12-11 16:37:35 -0600

    Probando ultima version de router

---
+++ src/router/index.jsx
@@ -0,0 +1,40 @@
+import {createBrowserRouter} from "react-router-dom"
+import LayoutPublic from "../layout/LayoutPublic"
+import About from "../pages/About"
+import Blog, { loaderBlog } from "../pages/Blog"
+import Home from "../pages/Home"
+import NotFound from "../pages/NotFound"
+import Post, { loaderPost } from "../pages/Post"
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LayoutPublic />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                errorElement: <NotFound />,
+                children: [
+                    {
+                        index: true,
+                        element: <Home />,
+                    },
+                    {
+                        path: "/about",
+                        element: <About />,
+                    },
+                    {
+                        path: "/blog",
+                        element: <Blog />,
+                        loader: loaderBlog
+                    },
+                    {
+                        path: "/blog/:id",
+                        element: <Post />,
+                        loader: loaderPost
+                    },
+                ]
+            },
+        ],
+    },
+]);
